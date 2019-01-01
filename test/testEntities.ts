65b17731ab85f4b7f19f7e2bf40965951c900d33
W W <wouter@1twaalf.nl>
2019-01-01 20:12:50 +0100

    update test entities

--- test/testEntities.ts
+++ test/testEntities.ts
@@ -3,29 +3,40 @@ import { Column, Entity } from '../src/decorators';
 
 @Entity('regions')
 export class Region {
+    @Column({ primary: true })
     public id!: string;
+    @Column()
     public code: number;
 }
 
 @Entity('userCategories')
 export class UserCategory {
+    @Column({ primary: true })
     public id!: string;
+    @Column()
     public name!: string;
     @Column()
     public region!: Region;
+    @Column()
     public regionId!: string;
+    @Column()
     public year!: number;
 }
 
 
 @Entity('users')
 export class User {
+    @Column({ primary: true })
     public id!: string;
+    @Column()
     public name!: string;
+    @Column()
     public numericValue: number;
+    @Column()
     public someValue!: string;
     @Column()
     public category!: UserCategory;
+    @Column()
     public categoryId!: string;
     @Column()
     public category2!: UserCategory;
@@ -33,18 +44,22 @@ export class User {
 }
 
 
-
 @Entity('userSettings')
 export class UserSetting {
-    @Column()
+    @Column({ primary: true })
     public id!: string;
     @Column()
     public user!: User;
+    @Column()
     public userId!: string;
     @Column()
     public user2!: User;
+    @Column()
     public user2Id!: string;
+    @Column()
     public key!: string;
+    @Column()
     public value!: string;
+    @Column()
     public initialValue!: string;
 }
