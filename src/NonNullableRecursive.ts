a3bbd3d8e1e40cd378f6db55ecea50db8a0e6268
W W <wouter@1twaalf.nl>
2020-02-05 10:48:52 +0100

    prettier

--- src/NonNullableRecursive.ts
+++ src/NonNullableRecursive.ts
@@ -1,4 +1,4 @@
-import { NonForeignKeyObjects } from "./NonForeignKeyObjects";
+import { NonForeignKeyObjects } from './NonForeignKeyObjects';
 // Removes all optional, undefined and null from type
 export type NonNullableRecursive<T> = {
     [P in keyof T]-?: T[P] extends object ? T[P] extends NonForeignKeyObjects ? Required<NonNullable<T[P]>> : NonNullableRecursive<T[P]> : Required<NonNullable<T[P]>>;
