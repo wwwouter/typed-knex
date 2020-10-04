827b0eaef0967e0272e2c26497dcf9f06cea81ec
W W <wouter@1twaalf.nl>
2020-10-04 19:10:54 +0200

    fix NonNullableRecursive

--- src/NonNullableRecursive.ts
+++ src/NonNullableRecursive.ts
@@ -1,5 +1,5 @@
 import { SelectableColumnTypes } from './SelectableColumnTypes';
 // Removes all optional, undefined and null from type
 export type NonNullableRecursive<T> = {
-    [P in keyof T]-?: T[P] extends SelectableColumnTypes ? Required<NonNullable<T[P]>> : NonNullableRecursive<T[P]>;
+    [P in keyof T]-?: T[P] extends SelectableColumnTypes ? NonNullable<T[P]> : NonNullableRecursive<NonNullable<T[P]>>;
 };
