cfae678455af3edc78fd0105dd48ee0d25e949b6
Wouter Mooij <wouter@1twaalf.nl>
2022-03-17 09:22:13 +0100

    upgrade

--- src/cli.ts
+++ src/cli.ts
@@ -1,19 +1,23 @@
 #!/usr/bin/env node
-import * as yargs from 'yargs';
-import { runUpgrade } from './upgrade/upgradeRunner';
+import * as yargs from "yargs";
+import { runUpgrade } from "./upgrade/upgradeRunner";
 
-const argv = yargs
-    .usage('Usage: $0 [-p tsconfig.json][-u upgrade] action(s)')
-    .options({ u: { type: 'boolean' }, p: { type: 'string' } })
-    .describe({ u: 'Run upgrader', h: 'Display this help message' })
-    .boolean(['u', 'h'])
-    .help()
-    .alias('h', 'help').argv;
+async function main() {
+    const argv = await yargs
+        .usage("Usage: $0 [-p tsconfig.json][-u upgrade] action(s)")
+        .options({ u: { type: "boolean" }, p: { type: "string" } })
+        .describe({ u: "Run upgrader", h: "Display this help message" })
+        .boolean(["u", "h"])
+        .help()
+        .string("_")
+        .alias("h", "help").argv;
 
-if (argv.u) {
-    // tslint:disable-next-line: no-floating-promises
-    runUpgrade(argv._, argv.p);
-
-} else {
-    yargs.showHelp();
+    if (argv.u) {
+        // tslint:disable-next-line: no-floating-promises
+        runUpgrade(argv._ as string[], argv.p);
+    } else {
+        yargs.showHelp();
+    }
 }
+
+main();
